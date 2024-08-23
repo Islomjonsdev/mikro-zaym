d86bfb8fb40682cd33da72f35fb48d2c3c509d56
Islomjonsdev <islomjon@immigrateable.com>
2024-08-23 16:19:39 +0500

    footer done

--- src/app/Footer/Footer.jsx
+++ src/app/Footer/Footer.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import "./Footer.scss";
+import FooterBottom from "./FooterBottom/FooterBottom";
 
 const Footer = () => {
+  const footerTopData = [
+    {
+      number: "0570",
+      email: "Колл-центр",
+    },
+    {
+      number: "+998 90 123 - 45 - 67",
+      email: "Телефон доверия:",
+    },
+    {
+      number: "company@name.com",
+      email: "Эллектрон почта:",
+    },
+  ];
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="container">
+        <div className="footer_blog">
+          <div className="footer_top">
+            <div className="footer_top_left">
+              {footerTopData?.map((item, index) => {
+                return (
+                  <div>
+                    <Link href={"/"}>{item?.number}</Link>
+                    <p>{item?.email}</p>
+                  </div>
+                );
+              })}
+            </div>
 
-export default Footer
+            <ul className="footer_top_right">
+              <li className="footer_right_item">
+                <h3>Частным лицам</h3>
+                <ul className="footer_right_list">
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Партнеры</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Карты</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Вклады</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Международные переводы</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Пользовательское соглашение</Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="footer_right_item">
+                <h3>Бизнесу</h3>
+
+                <ul className="footer_right_list">
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Расчетное обслуживание</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Карты</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Кредиты</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Депозиты</Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="footer_right_item">
+                <h3>Услуги</h3>
+
+                <ul className="footer_right_list">
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Автокредит</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Ипотека</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Потребительские кредиты</Link>
+                  </li>
+                  <li className="footer_right_item">
+                    <Link href={"/"}>Дебетовая карта</Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <FooterBottom />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/Footer/FooterBottom/FooterBottom.jsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React from "react";
+import { Icons } from "../../SvgIcons/index";
+import "./FooterBottom.scss";
+
+const FooterBottom = () => {
+  return (
+    <div className="footer_bottom">
+      <div className="footer_bottom_left">
+        <div className="footer_bottom_left_wrapper">
+          <div className="footer_bottom_left_info">
+            <Link href={"/"}>
+              <Icons.instagramm className="instagram" />
+            </Link>
+            <Link href={"/"}>
+              <Icons.telegram className="telegram" />
+            </Link>
+            <Link href={"/"}>
+              <Icons.facebook className="facebook" />
+            </Link>
+          </div>
+
+          <p>© 2024 ООО «Банк «Get it».</p>
+        </div>
+      </div>
+
+      <div className="footer_bottom_right">
+          <p>Мобильное приложение:</p>
+          <Link href={"/"}>
+            <Icons.apple className="apple"/>
+          </Link>
+          <Link href={"/"}>
+            <Icons.android className="android"/>
+          </Link>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBottom;
