e50074ba6f4eabf2ca0393674730cb0334911656
Islomjonsdev <islomjon@immigrateable.com>
2024-08-28 10:23:42 +0500

    hero done

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import heroImg from "../../../../public/images/heroimg.png"
+import "./Hero.scss"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero'>
+        <div className='container'>
+            <div className='hero_container'>
+                <div className='hero_left'>
+                      <h2>Вклады в национальной <span>валюте</span></h2>
+                      <Link href={"/"}>подробнее</Link>
+                </div>
+                <div className='hero_right'>
+                  <Image src={heroImg} alt={"heroImg"}/>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
