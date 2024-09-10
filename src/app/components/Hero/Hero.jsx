0c43c705aec0f735f9ca4cc2a804d559e11877b1
Islomjonsdev <islomjon@immigrateable.com>
2024-09-10 09:54:33 +0500

    hero responsive done

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,25 +1,27 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import heroImg from "../../../../public/images/heroimg.png"
-import "./Hero.scss"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import heroImg from "../../../../public/images/heroimg.png";
+import "./Hero.scss";
 
 const Hero = () => {
   return (
-    <section className='hero'>
-        <div className='container'>
-            <div className='hero_container'>
-                <div className='hero_left'>
-                      <h2>Вклады в национальной <span>валюте</span></h2>
-                      <Link href={"/"}>подробнее</Link>
-                </div>
-                <div className='hero_right'>
-                  <Image src={heroImg} alt={"heroImg"}/>
-                </div>
-            </div>
+    <section className="hero">
+      <div className="container">
+        <div className="hero_container">
+          <div className="hero_left">
+            <h2>
+              Вклады в национальной <span>валюте</span>
+            </h2>
+          </div>
+          <div className="hero_right">
+            <Image src={heroImg} alt={"heroImg"} />
+          </div>
         </div>
+            <Link href={"/"}>подробнее</Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
